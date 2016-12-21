e0229db7166a7517fc32b2b8ec9d144aaf07f2a1
Kiril_Velichkov <kvelichkow@gmail.com>
2016-12-21 22:45:52 +0200

    Кико JR check /server/config/config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,6 +8,7 @@ module.exports = {
     development: {
         rootPath: rootPath,
         connectionString: 'mongodb://Admin:junkies1234@ds141098.mlab.com:41098/funfact-project',
+        // connectionString: 'localhost:27017', //use this if you are in the holy Academy 
         port: PORT
     },
     production: {
