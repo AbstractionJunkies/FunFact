0bdb57a6c7eafcd5c2b3c3ac7eadbe6581e90fe3
RuzmanovDev <stoyan.ruzmanov@gmail.com>
2016-12-23 21:56:40 +0200

    added auth with jwt

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
-
-const RegisterUrl = 'http://localhost:1337/api/auth/register';
-const LoginUrl = 'http://localhost:1337/api/auth/login';
 import 'rxjs/add/operator/map'
+
+const RegisterUrl: string = 'http://localhost:1337/api/auth/register';
+const LoginUrl: string = 'http://localhost:1337/api/auth/login';
+const AuthToken: string = 'auth_token';
 
 @Injectable()
 export class AuthenticationService {
+
+    private loggedIn = false;
 
     constructor(private _http: Http) {
 
@@ -23,7 +26,19 @@ export class AuthenticationService {
     login(userToLogin: Object): Observable<any> {
         return this._http.post(LoginUrl, userToLogin)
             .map((res: Response) => {
-                return { status: res.status, body: res.json() }
+                let body = res.json();
+                let token = body.token;
+                console.log(body.token);
+                localStorage.setItem(AuthToken, token);
+                return { status: res.status, body: body }
             });
+    }
+
+    logout(): void {
+        localStorage.removeItem(AuthToken);
+    }
+
+    isLoggedIn(): boolean {
+        return this.loggedIn;
     }
 }
